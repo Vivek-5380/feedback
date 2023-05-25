21dab65619b83cf8e73cefc600fa3f02d1f8fa66
Vivek kr <vivekkr0004@gmail.com>
2023-05-25 08:48:12 +0530

    Added option for image upload

--- feedback/settings.py
+++ feedback/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiles',
     'reviews',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/' 
 # STATICFILES_DIRS = []
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/user-media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
